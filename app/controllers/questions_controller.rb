3529ce0faf5b27be7ee5ea8d4b79e4d7844ecf8d
Foo Xiaoxi <fooxiaoxi@gmail.com>
2022-11-08 15:11:19 +0800

    rails stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,17 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @question = params[:question]
+    if @question == 'hello'
+      @answer = 'Hhaha'
+    elsif @question == 'what time is it?'
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @question == ''
+      @answer = "I don't care, get dressed and go to work!!"
+    else
+      @answer = "I don't care, get dressed and go to work!!"
+    end
+  end
 end
